3eeb34ca05bc23f24e199b3c51f76a457f7c9f84
Nic Johnson <iheartredsox@gmail.com>
2019-07-08 14:22:35 -0600

    Add config

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+
+$channels = array(
+  'Alex' => array(
+    'mail' => 'alex.tyler@wgu.edu',
+    'slack_id' => 'UHWB7FENN',
+  ),
+  'Gaurav' => array(
+    'mail' => 'gaurav.shinde@wgu.edu',
+    'slack_id' => 'UJ1ELPVQ9',
+  ),
+  'JJ' => array(
+    'mail' => 'jj.berrett@wgu.edu',
+    'slack_id' => 'UL3A51CMV',
+  ),
+  'Nic' => array(
+    'mail' => 'nic.johnson@wgu.edu',
+    'slack_id' => 'UHD96EWRW',
+  ),
+);
+
+$slack_token = 'xoxp-594062572741-683345046743-681724783585-5eebcaa5a8ba203c3d6fa0bfc5689f78';
+
+$nexmo_key = array(
+  'api_key' => '',
+  'api_secret' => '',
+);
+
+$userlist_password = '';
